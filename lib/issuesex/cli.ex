d2913e2a12ed3855a16092625f1272676b47b7d4
chansuke <chansuke@georepublic.co.jp>
2017-04-10 23:39:29 +0900

    Add helper

--- lib/issuesex/cli.ex
+++ lib/issuesex/cli.ex
@@ -1,13 +1,25 @@
 defmodule Issuesex.CLI do
   @default_count 5
 
-  @moduledoc
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last _n_ issues in a github project
+  """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
-  @doc
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.
+  Return a tuple of `{ user, project, count }`, or `:help` if help was given.
+  """
 
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [ help: :boolen],
@@ -20,5 +32,16 @@ defmodule Issuesex.CLI do
     { _,  [ user, project ],        _ } -> { user, project, @default_count }
     _                                   -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts"""
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
