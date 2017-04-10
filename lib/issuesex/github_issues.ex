1b7be1086ade9596e004df6e72fffb4c3672b186
chansuke <chansuke@georepublic.co.jp>
2017-04-11 01:42:06 +0900

    Enable to sort data by

--- lib/issuesex/github_issues.ex
+++ lib/issuesex/github_issues.ex
@@ -19,23 +19,4 @@ defmodule Issues.GithubIssues do
   def handle_response({ ___, %{status_code: ___, body: body}}) do
     { :error, Poison.Parser.parse!(body) }
   end
-
-  def process({user, project, _count}) do
-    Issues.GithubIssues.fetch(user, project)
-    |> decode_response
-    |> convert_to_list_of_maps
-  end
-
-  def decode_response({:ok, body}), do: body
-
-  def decode_response({:error, error}) do
-    {_, message} = List.keyfind(error, "message", 0)
-    IO.puts "Error fetching from Github: #{message}"
-    System.halt(2)
-  end
-
-  def convert_to_list_of_maps(list) do
-    list
-    |> Enum.map(&Enum.into(&1, Map.new))
-  end
 end
