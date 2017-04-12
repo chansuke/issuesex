1246bbea258f69099a7ed04d4aa6c091e18bf961
chansuke <chansuke@georepublic.co.jp>
2017-04-12 23:37:39 +0900

    Add test for comment

--- mix.exs
+++ mix.exs
@@ -2,13 +2,15 @@ defmodule Issuesex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :issuesex,
-     escript:         escript_config,
-     version:         "0.0.1",
-     elixir:          "~> 1.2",
-     build_embedded:  Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps:            deps]
+    [ app:             :issuesex,
+      escript:         escript_config,
+      version:         "0.0.1",
+      elixir:          "~> 1.2",
+      build_embedded:  Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      name:            "Issuesex",
+      source_url:      "https://github.com/chansuke/issuesex",
+      deps:            deps ]
   end
 
   def application do
@@ -18,7 +20,9 @@ defmodule Issuesex.Mixfile do
   defp deps do
     [ 
       httpoison:  "~> 0.8",
-      poison:     "~> 1.5"
+      poison:     "~> 1.5",
+      ex_doc:     "~> 0.11",
+      earmark:    ">= 0.0.0"
     ]
   end
 
